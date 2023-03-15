e4f463bfe15965143631c032df0d94bbb261002e
HsingTzuPeng <glen65218@gmail.com>
2023-03-15 10:23:38 +0800

    debug_1

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.list import ListView
+from django.views.generic.edit import FormView
+from .models import Product, Order, OrderItem
 
 # Create your views here.
 
